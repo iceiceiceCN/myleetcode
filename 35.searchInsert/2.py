17456f28835462ccd0ca7516193471e40db9b6ac
iceiceiceCN <iceiceiceCN@gmail.com>
2020-09-09 17:00:53 +0800

    update 35

--- 35.searchInsert/2.py
+++ 35.searchInsert/2.py
@@ -1,25 +1,26 @@
-def searchInsert(nums, target) :
-    size = len(nums)
+class Solution:
+    def searchInsert(nums, target) :
+        size = len(nums)
 
-    if size == 0:
-        return 0
-    
-    if target > nums[size-1]:
-        return size
+        if size == 0:
+            return 0
+        
+        if target > nums[size-1]:
+            return size
 
-    left = 0
-    right = size - 1
+        left = 0
+        right = size - 1
 
-    while left < right:
-        mid = (left + right)//2
-        if nums[mid] < target:
-            left = mid + 1
-        elif nums[mid] > target:
-            right = mid
-        elif nums[mid] == target:
-            return mid
+        while left < right:
+            mid = (left + right)//2
+            if nums[mid] < target:
+                left = mid + 1
+            elif nums[mid] > target:
+                right = mid
+            elif nums[mid] == target:
+                return mid
 
-    return left
+        return left
 
 
 
