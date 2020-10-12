7ee076521e9c034720337071a3e9f46c212232de
iceiceiceCN <iceiceice@gmail.com>
2020-10-12 23:11:18 +0800

    更新35.2 注释

--- 35.searchInsert/2.py
+++ 35.searchInsert/2.py
@@ -11,7 +11,7 @@ class Solution:
         left = 0
         right = size - 1
 
-        while left < right:
+        while left < right: # 退出循环时,left 与 right 重合,[left, right]只剩下1个元素
             mid = (left + right)//2
             if nums[mid] < target:
                 left = mid + 1
@@ -19,9 +19,15 @@ class Solution:
                 right = mid
             elif nums[mid] == target:
                 return mid
+        # 如果找到目标值则返回索引; 
+        # 如果找不到目标值,left 为最接近目标值的,即插入位置.
+        return left 
 
-        return left
-
-
+"""
+二分搜索思路:
+当题目中有隐含的单调性时,可以思考使用二分搜索.
+    划分[left, mid]与[mid + 1, right],mid 被分到左边,对应 int mid = left + (right - left) / 2
+    划分[left, mid - 1]与[mid, right],mid 被分到右边,对应 int mid = left + (right - left + 1) /2
+"""
 
     
