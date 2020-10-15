d6995ef6f3c8ac6ad5b6bf3d29c6acfcb2ebfd7a
iceiceiceCN <iceiceiceCN@gmail.com>
2020-10-15 11:22:15 +0800

    update 283 1.py 注释

--- 283.moveZeroes/1.py
+++ 283.moveZeroes/1.py
@@ -6,8 +6,8 @@ class Solution(object):
         """
         zero_count = nums.count(0)
         start = 0
-        for i in nums:
-            if i == 0:
+        for i in nums: # 不要用range(len(nums)),使用索引会很麻烦，因为要删去元素，下标不好控制，直接从nums里抽值就行
+            if i == 0 and start < zero_count:
                 nums.remove(i)
                 nums.append(0)
                 start += 1
