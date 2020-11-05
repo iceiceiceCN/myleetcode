57faeb5908926b4fa38a3ca92f68449b982157ac
iceiceiceCN <iceiceiceCN@gmail.com>
2020-11-05 09:55:20 +0800

    update 415

---
+++ 415.addStrings/1.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def addStrings(self, num1, num2):
+        """
+        :type num1: str
+        :type num2: str
+        :rtype: str
+        """
+        if len(num1) < len(num2): # 保证num1长度大
+            num1,num2=num2,num1
+        num2 = '0'*(len(num1)-len(num2)) + num2
+        res = ''
+        summ = 0
+        for i in range(len(num1)):
+            n1 = int(num1[-i-1])
+            n2 = int(num2[-i-1])
+            res = str((n1+n2+summ)%10) + res
+            summ = (n1+n2+summ)//10
+            
+
+        if summ == 1:
+            res = '1' + res
+        return res
+
+if __name__ == "__main__":
+    A = Solution()
+    B = A.addStrings("9","99")
+
+# 参照67题
