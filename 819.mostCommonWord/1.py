d128f9220ee4c29ff10e4849f1d1dafceb90bf8a
iceiceiceCN <iceiceiceCN@gmail.com>
2020-11-11 14:23:01 +0800

    update 819

--- 819.mostCommonWord/1.py
+++ 819.mostCommonWord/1.py
@@ -1,4 +1,6 @@
 from collections import Counter
+
+
 class Solution(object):
     def mostCommonWord(self, paragraph, banned):
         """
@@ -7,20 +9,22 @@ class Solution(object):
         :rtype: str
         """
         for c in "!?',;.":
-            paragraph = paragraph.replace(c," ")
+            paragraph = paragraph.replace(c, " ")
 
         par = paragraph.lower().split()
-        
+
         res = Counter(par)
         max = -1
         flag = -1
-        for k,v in res.items(): # 使用Counter的时候要用 k,v .items()
-            if v>max and k not in banned:
+        for k, v in res.items():  # 使用Counter的时候要用 k,v .items()
+            if v > max and k not in banned:
                 max = v
                 flag = k
-        
+
         return flag
+
 
 if __name__ == "__main__":
     a = Solution()
-    b = a.mostCommonWord(paragraph = "Bob hit a ball, the hit BALL flew far after it was hit.",banned = ["hit"])
+    b = a.mostCommonWord(
+        paragraph="Bob hit a ball, the hit BALL flew far after it was hit.", banned=["hit"])
