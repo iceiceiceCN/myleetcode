f65dc26dbe760f39d4a924b6de95abd465adb178
iceiceiceCN <iceiceiceCN@gmail.com>
2020-11-24 16:03:19 +0800

    update

---
+++ 107.levelOrderBottom/2.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def levelOrderBottom(self, root):
+        if not root:
+            return []
+        queue = [root]
+        # 改用双端队列实现，每次都插入到第一位
+        res = collections.deque()
+        while queue:
+            size = len(queue)
+            tmp = []
+            for _ in xrange(size):
+                node = queue.pop(0)
+                tmp.append(node.val)
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            # 每次插入到第一位，这样自带了翻转的功能       
+            res.appendleft(tmp)
+        return list(res)
