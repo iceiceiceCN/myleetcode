c3d5b58ad40a69514d3ab39a86b6e09da2f5a6e1
iceiceicecn <iceiceicecn@gmail.com>
2020-12-28 09:20:10 +0800

    update 617

--- 617.@merge-two-binary-trees/BFS.py
+++ 617.@merge-two-binary-trees/BFS.py
@@ -10,13 +10,16 @@ class Solution(object):
     # 如果 t1和t2中，只要有一个是null，函数就直接返回
         if not (t1 and t2):
             return t2 if not t1 else t1
+
         queue = [(t1, t2)]
         while queue:
-            # print(a for a in queue)
             r1, r2 = queue.pop(0)
+
             r1.val += r2.val
+
             # 如果r1和r2的左子树都不为空，就放到队列中
             # 如果r1的左子树为空，就把r2的左子树挂到r1的左子树上
+            # 因为是以 r1 为准，也就是把r1、r2的内容都合并到r1上，最后返回r1；如果r1的不空，但是r2是空的，相当于什么都不做，也就不用判断了 只要判断r1是空的，r2不空就可以了，这时候把r2赋给r1即可
             if r1.left and r2.left:
                 queue.append((r1.left, r2.left))
             elif not r1.left:
@@ -25,7 +28,8 @@ class Solution(object):
             if r1.right and r2.right:
                 queue.append((r1.right, r2.right))
             elif not r1.right:
-                r1.right = r2.right
+                r1.right = r2.right # 会把r2的整个右子树都接到r1上去
+                
         return t1
 
 if __name__ == "__main__":
