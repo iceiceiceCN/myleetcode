cee778c3e0abc3fa381bfe5557291cba62644323
iceiceicecn <iceiceicecn@gmail.com>
2021-01-03 11:22:09 +0800

    update 876

--- 876.middle-of-the-linked-list/2.py
+++ 876.middle-of-the-linked-list/2.py
@@ -1,3 +1,5 @@
+import sys
+sys.path.append("F:\myleetcode\LocalTest")
 from ListNodeTest import *
 
 class Solution:
--- LocalTest/ListNodeTest.py
+++ LocalTest/ListNodeTest.py
@@ -4,46 +4,36 @@ class ListNode():
         self.val = x
         self.next = None
 #将传入的数组转化为链表
-def create_linked_list(arr):
-    head = ListNode(arr[0])
+def create_linked_list(nums):
+    if len(nums) == 0:
+        return None
+    head = ListNode(nums[0])
     cur = head
-    for i in range(1, len(arr)):
-        cur.next = ListNode(arr[i])
+    for i in range(1, len(nums)):
+        cur.next = ListNode(nums[i])
         cur = cur.next
     return head
+
 #传入链表头节点，以数组形式返回
-def print_linked_list(head):
-    cur = head
-    res = []
+def print_linked_list(list_node):
+    if list_node is None:
+        return
+
+    cur = list_node
     while cur:
-        res.append(cur.val)
+        print(cur.val, '->', end=' ')
         cur = cur.next
-    return res
+    print('null')
 
 
 
 """
-class Solution():
-    def mergeTwoLists(self, l1, l2):
-        pre = ListNode(0)
-        head = pre
-        while l1 and l2:
-            if l1.val >= l2.val:
-                pre.next = l2
-                l2 = l2.next
-            else:
-                pre.next = l1
-                l1 = l1.next
-            pre = pre.next
-        pre.next = l1 if l1 else l2
-        return head.next
-
-
-if __name__ == "__main__":
-    head1 = create_linked_list([1, 2, 4])
-    head2 = create_linked_list([1, 3, 4])
+if __name__ == '__main__':
+    # nums = [1, 2, 3, 4, 5, 6, 7]
+    nums = [1, 2, 3, 4, 5, 6, 7, 8]
+    head = create_linked_list(nums)
     solution = Solution()
-    sorted_lists = solution.mergeTwoLists(head1, head2)
-    print(print_linked_list(sorted_lists))
+    result = solution.middleNode(head)
+    print('结果：')
+    print_linked_list(result)
 """
-#输出：[1, 1, 2, 3, 4, 4]
