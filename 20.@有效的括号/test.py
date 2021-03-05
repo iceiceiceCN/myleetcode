e51b1e0f1747ed90deb41e6dcb9077a57c52a14a
iceiceicecn <iceiceicecn@gmail.com>
2021-03-05 20:46:01 +0800

    update @

--- 20.@有效的括号/test.py
+++ 20.@有效的括号/test.py
@@ -1,10 +1,18 @@
-A = []
-if not A: # 如果A空
-    print('A is None')
-else:
-    print("A is not None")
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        dic = {'(':')','{':'}','[':']'}
 
-# if A is not None:
-#     print('A is not None')
-# else:
-#     print('A is None ')
+        for i in s:
+            if i in dic:
+                stack.append(i)
+            
+            else:
+                if not stack or i != dic[stack[-1]]:
+                    return False
+                stack.pop()
+        
+        return not stack
+
+a = Solution()
+b = a.isValid("()")
